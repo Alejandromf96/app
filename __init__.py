1b37efb7a39783137c808b3bfdda672c7d531f5a
Alejandromf96 <93749273+Alejandromf96@users.noreply.github.com>
2024-06-14 15:40:33 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,14 +5,10 @@ import portfolio
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_mapping(
-        SENGRID_KEY=os.environ.get('SENDGRID_KEY'),
-    )
-
     app.register_blueprint(portfolio.bp)
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=0.0.0.0)
